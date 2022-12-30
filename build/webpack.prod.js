273e5754da12ae0f6b457f3df920f572d63d54c4
zhanghongan <1838344185@qq.com>
2022-12-30 17:40:15 +0800

    配置完毕

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -6,11 +6,17 @@ const copyPlugin = require('copy-webpack-plugin')
 
 const path =require('path')
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 const TerserPlugin = require('terser-webpack-plugin')
+
+const globAll = require('glob-all')
+
+const PurgeCSSPlugin=require('purgecss-webpack-plugin')
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = merge(baseConfig,{
     mode:'production',//生产模式，会开启tree-shaking和压缩代码，以及其他优化
@@ -29,6 +35,28 @@ module.exports = merge(baseConfig,{
         }),
         new MiniCssExtractPlugin({
             filename:'static/css/[name][contenthash:8].css'//抽离css的输出目录和名称
+        }),
+        // 清理无用css
+        new PurgeCSSPlugin({
+            // 检测src下所有tsx文件和public下index.html中使用的类名和id和标签名称
+            // 只打包这些文件中用到的样式
+            paths:globAll.sync([
+                `${path.join(__dirname,'../src')}/**/*.tsx`,
+                path.join(__dirname,'../public/index.html')
+
+            ]),
+            safelist:{
+                standard:[/^ant-/],//过滤以ant-开头的类名，哪怕没用到也不删除
+            }
+        }),
+        new CompressionPlugin({
+            test:/.(js|css)$/,//只生成css,js压缩文件
+            filename:'[path][base].gz',//文件命名
+            algorithm:'gzip',//压缩格式，默认是gzip
+            test:/.(js|css)$/,//只生成css，js压缩文件
+            threshold:10240,//只有大小大于该值的资源会被处理，默认值是10k
+            minRatio:0.8//压缩率，默认值是0.8
+            
         })
     ],
     optimization:{
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React,{useState} from 'react'
-import '@/app.css'
+import React,{lazy,Suspense,useState} from 'react'
+// import '@/app.css'
 import '@/app.less'
 import '@/app.scss'
 import smallImg from '@/assets/images/4kb.png'
 import bigImg from '@/assets/images/22kb.png'
+import {Demo1,Demo2} from '@/components'
+const LazyDemo = lazy(()=>import('@/components/lazyDemo'))//使用import语法配合react的lazy动态引入资源
 
 function App(){
     const [count ,setCounts] = useState('')
+    const [show,setShow] = useState(false)
     const onChange=(e:any)=>{
         setCounts(e.target.value)
+    }
+    const onClick = ()=>{
+        import('./app.css')
+        setShow(true)
     }
     return (
         <>
@@ -22,6 +29,12 @@ function App(){
          <br />
          <p>非受控组件</p>
          <input type="text" />
+         <Demo1></Demo1>
+         <h2 onClick={onClick}>展示</h2>
+         {show&&
+         <Suspense fallback={null}>
+            <LazyDemo />
+        </Suspense>}
         </>
     )
 }
