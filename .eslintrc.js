9e39b935447755866b727746005337a47b3a0d55
zhanghongan <1838344185@qq.com>
2023-01-03 14:49:12 +0800

    更新eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,19 @@ module.exports = {
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 }
