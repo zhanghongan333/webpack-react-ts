4a5c16beb0de375e68b8febb1a011c066d556ff0
zhanghongan <1838344185@qq.com>
2023-01-03 18:02:47 +0800

    更新格式化数据

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
 import App from './App'
 
 const root = document.getElementById('root')
 if (root) {
-  createRoot(root).render(<App />)
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 }
+const a = 111
 console.log('NODE_ENV', process.env.NODE_ENV)
 console.log('BASE_ENV', process.env.BASE_ENV)
